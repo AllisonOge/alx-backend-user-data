9a0216e5fd098cafc19d94bd1178a9f175558cd2
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-07-04 22:28:54 +0000

    task#6: check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     hash a password using bcrypt
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    check if password is valid
+    """
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
