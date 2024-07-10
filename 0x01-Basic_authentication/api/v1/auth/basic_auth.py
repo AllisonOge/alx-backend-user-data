992117e3837687e981d4d225de33702bc026636b
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-07-10 01:40:06 +0000

    task#10: User object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,9 @@
 """
 Basic Authentication class
 """
+from typing import TypeVar
 from .auth import Auth
+from models.user import User
 import base64
 
 
@@ -46,3 +48,18 @@ class BasicAuth(Auth):
         if ":" not in decode_base64_authorization_header:
             return None, None
         return decode_base64_authorization_header.split(":")
+
+    def user_object_from_credentials(self, user_email:
+                                    str, user_pwd: str) -> TypeVar("User"):
+        """returns the User instance based on his email and password"""
+        if user_email is None or not isinstance(user_email, str):
+            return
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return
+        searches = User.search(dict(email=user_email))
+        if len(searches) == 0:
+            return
+        user = searches[0]
+        if not user.is_valid_password(user_pwd):
+            return
+        return user
