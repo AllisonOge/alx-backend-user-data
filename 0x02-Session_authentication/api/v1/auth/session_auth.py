9fe3bd22038b9a1c4cca82b48cf24c89769cb4a9
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-07-11 20:04:28 +0000

    task#6: use session id for identifying a User

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,9 @@
 """
 Module for the SessionAuth class
 """
+from typing import TypeVar
 from .auth import Auth
+from models.user import User
 import uuid
 
 
@@ -27,3 +29,10 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return
         return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar("User"):  # an overload function
+        """returns a `User` instance based on a cookie value"""
+        user_id = self.user_id_for_session_id(self.session_cookie(request))
+        if user_id is None:
+            return
+        return User.get(user_id)
