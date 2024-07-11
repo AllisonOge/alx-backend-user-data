dce4e811b3410e1cb076b49c982f82d01354b55d
Allison Ogechukwu <ogechukwukanu@gmail.com>
2024-07-11 06:34:52 +0000

    task#1: empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module for the SessionAuth class
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """class for creating a session authentication mechanism"""
+    pass
